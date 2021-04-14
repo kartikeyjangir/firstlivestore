1b4a312f32ed55a2a9020468077af07162ffaf69
kartikeyjangir <kartikeyjangir101001@gmail.com>
2021-04-14 13:44:16 +0530

    first

---
+++ server.js
@@ -0,0 +1,42 @@
+import express from "express";
+import mongoose from "mongoose";
+import path from "path";
+import dotenv from "dotenv";
+import productRouter from "./routers/productRouter.js";
+import userRouter from "./routers/userRouter.js";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+dotenv.config();
+const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/nehajangid", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+
+app.use(express.static(path.join(__dirname, "build")));
+app.use("/api/users", userRouter);
+app.use("/api/products", productRouter);
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", index.html));
+});
+
+app.get("/", (req, res) => {
+  res.send("Server is ready");
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server backend at port no. ${port}`);
+});
